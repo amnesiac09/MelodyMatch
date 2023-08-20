6764685f026836bcf88f180c5052e97c138f3ec4
Aleksandre Naneishvili <aleksandre.naneishvili@azry.com>
2023-08-20 15:55:21 +0400

    initial spring app

---
+++ src/main/java/ge/ananeishvililgorgadze/melodymatch/MelodyMatchApplication.java
@@ -0,0 +1,13 @@
+package ge.ananeishvililgorgadze.melodymatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodyMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/ananeishvililgorgadze/melodymatch/MelodyMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.ananeishvililgorgadze.melodymatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodyMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
