bd0317f6514f6dc938e5151d648e2b8e90e8f544
amnesiac09 <alexnaneishvili@gmail.com>
2023-09-03 18:36:20 +0400

    correct app in front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,7 @@ import {
   Footer
 } from "./components"
 import { logInUser } from './redux/actions/userActions';
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 import SockJS from "sockjs-client"
 
 function App() {
@@ -47,6 +47,47 @@ function App() {
     // }
   }, [location])
 
+  // const connection = useRef<WebSocket>();
+  // const shouldKeepWSAlive = useRef<boolean>(false);
+
+  useEffect(() => {
+    // Replace 'wss://your-websocket-server-url' with your WebSocket server URL
+    // const socket = new WebSocket('ws://localhost:8080/ws');
+
+    // // WebSocket event listeners
+    // socket.addEventListener('open', (event) => {
+    //   console.log('WebSocket connection opened:', event);
+    // });
+
+    // socket.addEventListener('message', (event) => {
+    //   console.log('WebSocket message received:', event.data);
+    //   // Handle incoming messages here
+    // });
+
+    // socket.addEventListener('close', (event) => {
+    //   console.log('WebSocket connection closed:', event);
+    // });
+
+    // Clean up the WebSocket connection when the component unmounts
+    // return () => {
+    //   socket.close();
+    // };
+    var sock = new SockJS('http://localhost:8080/ws');
+    sock.onopen = function() {
+      console.log('successful');
+      sock.send('test');
+    };
+
+    sock.onmessage = function(e) {
+      console.log('message', e.data);
+      sock.close();
+    };
+
+    sock.onclose = function(e) {
+      console.log(e)
+      console.log('unsuccessful');
+    };
+  }, []);
 
   // const socket = io.connect();
   // let a = new WebSocket("ws://localhost:8080/")
